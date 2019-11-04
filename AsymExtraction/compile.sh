ab4a2ec2718751e3b761558709547e92b3f04290
avossen <agvossen@gmail.com>
2019-11-04 15:56:24 -0500

    added sys debug

--- AsymExtraction/compile.sh
+++ AsymExtraction/compile.sh
@@ -6,10 +6,9 @@ echo cflags: $CFLAGS
 
 rootcint -f PlotRes_Dict.C -c  PlotResults.h
 
-
-#c++ $CFLAGS TwoHadAsymsCleaned.cc TwoHadAsymsCommons.cc MultiPlotter.cxx HadronPairArray.cxx PlotRes_Dict.C PlotResults.cxx -o TwoHadAsymsCMod
-clang $CFLAGS TwoHadAsymsCleaned.cc TwoHadAsymsCommons.cc MultiPlotter.cxx HadronPairArray.cxx PlotRes_Dict.C PlotResults.cxx -o TwoHadAsymsCMod 
-#c++ $CFLAGS CombPlots.cc TwoHadAsymsCommons.cc PlotRes_Dict.C MultiPlotter.cxx HadronPairArray.cxx PlotResults.cxx -o CombPlots
-clang $CFLAGS CombPlots.cc TwoHadAsymsCommons.cc PlotRes_Dict.C MultiPlotter.cxx HadronPairArray.cxx PlotResults.cxx -o CombPlots
+c++ $CFLAGS TwoHadAsymsCleaned.cc TwoHadAsymsCommons.cc MultiPlotter.cxx HadronPairArray.cxx PlotRes_Dict.C PlotResults.cxx -o TwoHadAsymsCMod
+#clang $CFLAGS TwoHadAsymsCleaned.cc TwoHadAsymsCommons.cc MultiPlotter.cxx HadronPairArray.cxx PlotRes_Dict.C PlotResults.cxx -o TwoHadAsymsCMod 
+c++ $CFLAGS CombPlots.cc TwoHadAsymsCommons.cc PlotRes_Dict.C MultiPlotter.cxx HadronPairArray.cxx PlotResults.cxx -o CombPlots
+#clang $CFLAGS CombPlots.cc TwoHadAsymsCommons.cc PlotRes_Dict.C MultiPlotter.cxx HadronPairArray.cxx PlotResults.cxx -o CombPlots
 #c++ $CFLAGS SysRatio.cc TwoHadAsymsCommons.cc PlotRes_Dict.C MultiPlotter.cxx HadronPairArray.cxx PlotResults.cxx -o SysRatio
 #c++ $CFLAGS TwoHadAsymsCleaned.cc TwoHadAsymsCommons.cc MultiFitter.cxx HadronQuadArray.cxx HadronPairArrays.cxx -o TwoHadAsymsC
